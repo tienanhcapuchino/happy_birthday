941ba28c6db64de153b94b758f8ada91ff406ea1
tienanhcapuchino <88329721+tienanhcapuchino@users.noreply.github.com>
2021-12-16 16:18:56 +0700

    big update

--- MyBirthday/birth.js
+++ MyBirthday/birth.js
@@ -9,3 +9,21 @@ function showbutton() {
         k++;
     }
 }
+function showQua() {
+    if (k % 2 == 0) {
+        document.getElementById('v1').style.display = 'block';
+        k++;
+    } else {
+        document.getElementById('v1').style.display = 'none';
+        k++;
+    }
+}
+function showQua2() {
+    if (k % 2 == 0) {
+        document.getElementById('v2').style.display = 'block';
+        k++;
+    } else {
+        document.getElementById('v2').style.display = 'none';
+        k++;
+    }
+}
