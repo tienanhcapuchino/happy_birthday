f29d969b865839627174156fa802f405549f403c
tienanhcapuchino <88329721+tienanhcapuchino@users.noreply.github.com>
2021-12-16 21:26:59 +0700

    update next

--- MyBirthday/birth.js
+++ MyBirthday/birth.js
@@ -10,20 +10,24 @@ function showbutton() {
     }
 }
 function showQua() {
-    if (k % 2 == 0) {
-        document.getElementById('v1').style.display = 'block';
-        k++;
-    } else {
+    if (k % 2 == 1) {
+        document.getElementById('qua1').innerHTML = 'Bấm vào đây dể lấy quà tiếp nào!';  
         document.getElementById('v1').style.display = 'none';
+        k++;
+    } else { 
+        document.getElementById('qua1').innerHTML = 'Bấm vào dể ẩn đi nhé!';
+        document.getElementById('v1').style.display = 'block';
         k++;
     }
 }
 function showQua2() {
-    if (k % 2 == 0) {
-        document.getElementById('v2').style.display = 'block';
-        k++;
-    } else {
+    if (k % 2 == 1) {
+        document.getElementById('qua2').innerHTML = 'Ơ vẫn còn nè, bấm tiếp đi!';  
         document.getElementById('v2').style.display = 'none';
+        k++;
+    } else { 
+        document.getElementById('qua2').innerHTML = 'Bấm vào dể ẩn đi nhé!';
+        document.getElementById('v2').style.display = 'block';
         k++;
     }
 }
