9fdb078d2bc8889568e3bc17ad960ab4acc6b205
tienanhcapuchino <88329721+tienanhcapuchino@users.noreply.github.com>
2021-12-21 21:59:08 +0700

    update final

--- MyBirthday/birth.js
+++ MyBirthday/birth.js
@@ -1,6 +1,7 @@
 
 var k = 0;
-function showbutton() {
+
+function showANH() {
     if (k % 2 == 0) {
         document.getElementById('anhchinhhap').style.display = 'block';
         k++;
@@ -9,25 +10,23 @@ function showbutton() {
         k++;
     }
 }
+
 function showQua() {
-    if (k % 2 == 1) {
-        document.getElementById('qua1').innerHTML = 'Bấm vào đây dể lấy quà tiếp nào!';  
-        document.getElementById('v1').style.display = 'none';
-        k++;
-    } else { 
-        document.getElementById('qua1').innerHTML = 'Bấm vào dể ẩn đi nhé!';
+    if (k % 2 == 0) {
         document.getElementById('v1').style.display = 'block';
+        k++;
+    } else {
+        document.getElementById('v1').style.display = 'none';
         k++;
     }
 }
+
 function showQua2() {
-    if (k % 2 == 1) {
-        document.getElementById('qua2').innerHTML = 'Ơ vẫn còn nè, bấm tiếp đi!';  
-        document.getElementById('v2').style.display = 'none';
-        k++;
-    } else { 
-        document.getElementById('qua2').innerHTML = 'Bấm vào dể ẩn đi nhé!';
+    if (k % 2 == 0) {
         document.getElementById('v2').style.display = 'block';
+        k++;
+    } else {
+        document.getElementById('v2').style.display = 'none';
         k++;
     }
 }
